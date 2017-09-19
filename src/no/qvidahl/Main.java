21894a4daa93831d18473829a1c1a7f412bc69fb
Thomas Qvidahl <thomasqvidahl@TQ-MBP.local>
2017-09-19 23:51:45 +0200

    Initial commit

---
+++ src/no/qvidahl/Main.java
@@ -0,0 +1,12 @@
+package no.qvidahl;
+
+/**
+ * This is my solution to the Sleeping Barber Problem
+ * Created by Thomas Qvidahl 19.09.2017
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
