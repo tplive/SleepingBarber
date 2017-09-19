58116d2942bb5f585e183398087c4f60d32705d2
Thomas Qvidahl <thomasqvidahl@TQ-MBP.local>
2017-09-19 23:53:51 +0200

    Problem definition, take one.. ;)

--- src/no/qvidahl/Main.java
+++ src/no/qvidahl/Main.java
@@ -3,10 +3,12 @@ package no.qvidahl;
 /**
  * This is my solution to the Sleeping Barber Problem
  * Created by Thomas Qvidahl 19.09.2017
+ * Problem wiki article: https://en.wikipedia.org/wiki/Sleeping_barber_problem
  */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Shh! The barber is sleeping!");
     }
 }
