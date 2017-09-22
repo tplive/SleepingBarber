1830531fcc0a3496d06c0ab02f5ebc8b8078b1f8
Thomas Qvidahl <tp@live.no>
2017-09-22 09:01:31 +0200

    Tuning. Commenting. Testing.

--- src/no/qvidahl/Sleepingbarber.java
+++ src/no/qvidahl/Sleepingbarber.java
@@ -10,42 +10,55 @@ import java.util.concurrent.Semaphore;
  */
 public class Sleepingbarber extends Thread {
 
+    // Uses Semaphores to lock access to concurrent resources
     public static Semaphore customers = new Semaphore(0);
     public static Semaphore barber = new Semaphore(0);
     public static Semaphore chairs = new Semaphore(1);
 
+    // Set barbershop capacity, initialize empty seats.
     private final static int capacity = 10;
     private static int freeSeats = capacity;
 
+    // Some counters
     private int servicedCustomers = 0;
     private int rejectedCustomers = 0;
     private int customerID = 1;
     private long totalWorkingTime = 0L;
     private long startTime = System.currentTimeMillis();
 
-
     private long runningTime() {
+        // Calculates running time of the script
         long now = System.currentTimeMillis();
         return now - startTime;
     }
 
     private static void msg(String msg) {
+        // Just simplifies writing to the console
         System.out.println(msg);
     }
 
     class Customer extends Thread {
 
+        // Threaded class for customers, they each get an ID:
         int id;
 
+        // Indicates that customer is impatiently waiting to be serviced:
         boolean waitingForService = true;
+
+        // Constructor, giving each customer their ID
         public Customer(int i) {
             this.id = i;
         }
 
+        // Method to get customer ID
         public int getCustId() {
             return this.id;
         }
 
+        // Async run method. Each customer loops and tries to get a lock on the chairs.
+        // If they do, and there are free seats available, they take a seat, and decrement
+        // the number of available free seats.
+        @Override
         public void run() {
             while(waitingForService) { // Loop as long as customer is waiting
                 try {
@@ -53,12 +66,12 @@ public class Sleepingbarber extends Thread {
                     if (freeSeats > 0 ) {
                         System.out.println("Customer " + getCustId() + " took seat " + freeSeats);
                         freeSeats--;
-                        customers.release();
-                        chairs.release();
+                        customers.release(); // If we have a lock on customers, release it.
+                        chairs.release(); // Release our lock on the chairs.
 
                         try {
                             barber.acquire(); // Get a lock on the barber, if available
-                            //this.getServiced(); // Service customer
+                            this.getServiced(); // Service customer
                             // So if we managed to complete service without interruption,
                             // customer can leave, so:
                             waitingForService = false; // Customer is leaving, never mind payment.. :P
@@ -68,11 +81,10 @@ public class Sleepingbarber extends Thread {
                     }else { // freeSeats = 0 = No room for customer
                         waitingForService = false; // customer leaves
                         rejectedCustomers++; // Another disappointed customer
-                        chairs.release();
+                        chairs.release(); // Let go of the chairs lock
                         System.out.println("No free seats available. Rejected " + rejectedCustomers + " customer(s)..");
                     }
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
             }
         }
@@ -95,6 +107,7 @@ public class Sleepingbarber extends Thread {
         public Barber() {
         }
 
+        @Override
         public void run() {
             while(true) {
 
@@ -119,14 +132,13 @@ public class Sleepingbarber extends Thread {
                 double workFor = Math.random() * 5000; // Work for x number of ms
                 msg("Barber is working for " + workFor + " ms..");
                 Thread.sleep((long) workFor); //(long) workFor); // Gotta get double to long somehow.. :P
-                servicedCustomers++;
-                totalWorkingTime += workFor;
+                servicedCustomers++; // When we're done, increment counter
+                totalWorkingTime += workFor; // Add work time to total
                 msg("Barber is done! Serviced " + servicedCustomers + " customer(s) so far..");
                 msg("Total working time so far: " + totalWorkingTime + " ms");
                 msg("Total running time so far: " + runningTime() + " ms");
 
             }catch(InterruptedException ie) {
-                msg("Barber was interrupted in his work! >:-O" + ie );
             }
         }
 
@@ -134,12 +146,17 @@ public class Sleepingbarber extends Thread {
 
     public static void main(String[] args) {
 
+        // This is where we set up shop!
         Sleepingbarber shop = new Sleepingbarber();
-
         shop.start();
 
      }
 
+    // Aync run method. We instantiate the barber, and start him looking for customers.
+    // Then we loop forever, instantiating new customers at random intervals.
+    // The barber is looping and trying to find customers as they arrive.
+    // When he is busy, they take a seat. If there are no seats, they leave.
+    @Override
     public void run() {
 
         Barber thomas = new Barber();
